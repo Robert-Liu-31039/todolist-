cf33391c93710f980068e4bf119bf35e6c99de80
robert <robert31039@gmail.com>
2025-05-14 21:07:09 +0800

    展示從 login 完成後跳轉至 todolist 頁面時，要如何取得登入的資訊

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from .models import Todo
 
 # Create your views here.
 def todolist(request):  # Django 規定 : 一定要帶 request 這個參數
+    user = request.user
 
     # .get() -> 回傳唯一物件，但並非容器型別
     todos = [Todo.objects.get(id=1)]
@@ -15,7 +16,7 @@ def todolist(request):  # Django 規定 : 一定要帶 request 這個參數
     # .all() -> 取得所有的資料，容器型別
     todos = Todo.objects.all()
 
-    result = {"todos": todos}
+    result = {"todos": todos, "user": user}
 
     return render(request, "todo/todolist.html", result)
 
