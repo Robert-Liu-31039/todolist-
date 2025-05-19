f3079a637109149b1e4008225423c63691c5c1fa
Robert-Liu-TW <robert31039@gmail.com>
2025-05-19 09:41:43 +0800

    1. 後端增加判定 : 如果有登入帳號的話，僅顯示登入者的 todo 內容 2. 前端增加判定 : 若無登入時，顯示請先登入或註冊

--- todo/views.py
+++ todo/views.py
@@ -7,14 +7,20 @@ from .models import Todo
 def todolist(request):  # Django 規定 : 一定要帶 request 這個參數
     user = request.user
 
-    # .get() -> 回傳唯一物件，但並非容器型別
-    todos = [Todo.objects.get(id=1)]
+    todos = None
 
-    # .filter() -> 回傳符合篩選的資料，容器型別
-    todos = Todo.objects.filter(id=1)
+    ## .get() -> 回傳唯一物件，但並非容器型別
+    # todos = [Todo.objects.get(id=1)]
 
-    # .all() -> 取得所有的資料，容器型別
-    todos = Todo.objects.all()
+    ## .filter() -> 回傳符合篩選的資料，容器型別
+    # todos = Todo.objects.filter(id=1)
+
+    ## .all() -> 取得所有的資料，容器型別
+    # todos = Todo.objects.all()
+
+    # 如果有登入帳號的話，僅顯示登入者的 todo 內容
+    if user.is_authenticated:
+        todos = Todo.objects.filter(user=request.user)
 
     result = {"todos": todos, "user": user}
 
